d0d4830edc78e1ae600bb5961e41666144699361
Ahmet Faruk Basaran <0aeon0@mail.com>
2025-11-16 20:42:27 +0300

    clean code v1

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: ahmbasar <ahmbasar@student.42istanbul.c    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/16 15:41:06 by ahmbasar          #+#    #+#             */
-/*   Updated: 2025/11/16 19:56:23 by ahmbasar         ###   ########.fr       */
+/*   Updated: 2025/11/16 20:38:24 by ahmbasar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,9 @@
 #define SURROUND	16
 
 #define PERM_DIM	24
+
+#define SUCCESS 0
+#define FAIL 1
 
 void	p_surround(char (*arr)[4][SIZE]);
 void	p_map(char (*map)[SIZE][SIZE]);
@@ -27,16 +30,29 @@ static const char	g_perms[PERM_DIM][SIZE] = {\
 	"1342", "3142", "2143", "1243", "4213", "2413", "1423", "4123" \
 	};
 
-void	init_arg(char (*arr)[4][SIZE], char *str)
+int	init_arg(char (*arr)[4][SIZE], char *str)
 {
-	int	i;
+	int	str_index;
+	int	num_index;
 
-	i = 0;
-	while (i < SURROUND)
+	str_index = 0;
+	while (str[str_index])
 	{
-		(*arr)[i / SIZE][i % SIZE] = str[i * 2];
-		i++;
+		num_index = str_index / 2;
+		if ((str_index % 2) == 0)
+		{
+			if (str[str_index] < '1' || str[str_index] > '4')
+				return (FAIL);
+			(*arr)[num_index / SIZE][num_index % SIZE] = str[str_index];
+		}
+		else
+			if (str[str_index] != ' ')
+				return (FAIL);
+		str_index++;
 	}
+	if (str_index != (32 - 1))
+		return (FAIL);
+	return (SUCCESS);
 }
 
 void	set_row(char const *perm, char *row)
@@ -48,34 +64,32 @@ void	set_row(char const *perm, char *row)
 		row[i] = perm[i];
 }
 
-void	s_map(char (*map)[SIZE][SIZE]);
-
-void	combinator(int ijkl[4], char (*map)[SIZE][SIZE], char (*arr)[4][SIZE])
+void	combinator(int max_24[4], char (*map)[SIZE][SIZE], char (*arr)[4][SIZE])
 {
-	while (ijkl[0] < PERM_DIM)
+	while (max_24[0] < PERM_DIM)
 	{
-		ijkl[1] = 0;
-		set_row(g_perms[ijkl[0]], (*map)[0]);
-		while (ijkl[1] < PERM_DIM)
+		max_24[1] = 0;
+		set_row(g_perms[max_24[0]], (*map)[0]);
+		while (max_24[1] < PERM_DIM)
 		{
-			ijkl[2] = 0;
-			set_row(g_perms[ijkl[1]], (*map)[1]);
-			while (ijkl[2] < PERM_DIM)
+			max_24[2] = 0;
+			set_row(g_perms[max_24[1]], (*map)[1]);
+			while (max_24[2] < PERM_DIM)
 			{
-				ijkl[3] = 0;
-				set_row(g_perms[ijkl[2]], (*map)[2]);
-				while (ijkl[3] < PERM_DIM)
+				max_24[3] = 0;
+				set_row(g_perms[max_24[2]], (*map)[2]);
+				while (max_24[3] < PERM_DIM)
 				{
-					set_row(g_perms[ijkl[3]], (*map)[3]);
+					set_row(g_perms[max_24[3]], (*map)[3]);
 					if (check(arr, map))
 						return ;
-					ijkl[3]++;
+					max_24[3]++;
 				}
-				ijkl[2]++;
+				max_24[2]++;
 			}
-			ijkl[1]++;
+			max_24[1]++;
 		}
-		ijkl[0]++;
+		max_24[0]++;
 	}
 }
 
@@ -86,7 +100,8 @@ int	main(int argc, char *argv[])
 
 	if (argc != 2)
 		return (write(2, "Error\n", 6));
-	init_arg(&arr, argv[1]);
+	if (init_arg(&arr, argv[1]))
+		return (write(2, "Error\n", 6));
 	combinator((int [4]){0, 0, 0, 0}, &map, &arr);
 	p_map(&map);
 	return (0);
--- utilize.c
+++ utilize.c
@@ -6,7 +6,7 @@
 /*   By: ahmbasar <ahmbasar@student.42istanbul.c    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/16 15:41:05 by ahmbasar          #+#    #+#             */
-/*   Updated: 2025/11/16 19:55:16 by ahmbasar         ###   ########.fr       */
+/*   Updated: 2025/11/16 20:41:09 by ahmbasar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,17 +15,8 @@
 #define SIZE		4
 #define SURROUND	16
 #define AREA		16
-
-// void	p_surround(char (*arr)[4][SIZE])
-// {
-// 	for (int i = 0; i < SURROUND; i++)
-// 	{
-// 		printf("-%d- ", (*arr)[i / SIZE][i % SIZE] - '0');
-// 		if ((i % SIZE) == 3)
-// 			printf("\n");
-// 	}
-// 	printf("\n");
-// }
+#define SUCCESS 1
+#define FAIL 0
 
 void	p_map(char (*map)[SIZE][SIZE])
 {
@@ -43,3 +34,67 @@ void	p_map(char (*map)[SIZE][SIZE])
 	}
 	write(1, "\n", 1);
 }
+
+int	check_dup_y(char c1, char c2, char c3, char c4)
+{
+	return (c1 == c2 || c1 == c3 || c1 == c4 || \
+			c2 == c3 || c2 == c4 || c3 == c4);
+}
+
+int	get_view(char a, char b, char c, char d)
+{
+	char	best;
+	int		views;
+
+	views = 1;
+	best = a;
+	if (best < b)
+	{
+		best = b;
+		views++;
+	}
+	if (best < c)
+	{
+		best = c;
+		views++;
+	}
+	if (best < d)
+		views++;
+	return (views + '0');
+}
+
+int	check(char (*surround)[4][SIZE], char (*map)[SIZE][SIZE])
+{
+	int	i;
+
+	i = -1;
+	while (++i < SIZE)
+	{
+		if (check_dup_y((*map)[0][i], (*map)[1][i], (*map)[2][i], (*map)[3][i]))
+			return (FAIL);
+		if (get_view((*map)[i][0], (*map)[i][1], (*map)[i][2], (*map)[i][3]) !=
+			(*surround)[2][i])
+			return (FAIL);
+		if (get_view((*map)[i][3], (*map)[i][2], (*map)[i][1], (*map)[i][0]) !=
+			(*surround)[3][i])
+			return (FAIL);
+		if (get_view((*map)[0][i], (*map)[1][i], (*map)[2][i], (*map)[3][i]) !=
+			(*surround)[0][i])
+			return (FAIL);
+		if (get_view((*map)[3][i], (*map)[2][i], (*map)[1][i], (*map)[0][i]) !=
+			(*surround)[1][i])
+			return (FAIL);
+	}
+	return (SUCCESS);
+}
+
+// void	p_surround(char (*arr)[4][SIZE])
+// {
+// 	for (int i = 0; i < SURROUND; i++)
+// 	{
+// 		printf("-%d- ", (*arr)[i / SIZE][i % SIZE] - '0');
+// 		if ((i % SIZE) == 3)
+// 			printf("\n");
+// 	}
+// 	printf("\n");
+// }
