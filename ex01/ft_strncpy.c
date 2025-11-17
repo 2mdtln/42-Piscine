7d0cbe57ad5da3d6eb1f3e6327620c54b3173558
2mdtln <120330170+2mdtln@users.noreply.github.com>
2025-11-17 17:36:55 +0300

    AAAAAAAAAAAAA

--- ex01/ft_strncpy.c
+++ ex01/ft_strncpy.c
@@ -6,7 +6,7 @@
 /*   By: mtaheri <mtaheri@student.42istanbul.com.tr>+#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/16 09:06:18 by mtaheri           #+#    #+#             */
-/*   Updated: 2025/11/17 13:55:10 by mtaheri          ###   ########.fr       */
+/*   Updated: 2025/11/17 17:35:43 by mtaheri          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,7 +22,7 @@ char	*ft_strncpy(char *dest, char *src, unsigned int n)
 	}
 	while (x < n)
 	{
-		dest[n] = '\0';
+		dest[x] = '\0';
 		x++;
 	}
 	return (dest);
