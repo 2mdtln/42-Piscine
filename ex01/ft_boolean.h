4cbd50b8b8bc0d2e67589ad2906b29f55c0a221e
2mdtln <120330170+2mdtln@users.noreply.github.com>
2025-11-27 15:33:37 +0300

    fix(C08): Ex01 Ex02 (not tested)

--- ex01/ft_boolean.h
+++ ex01/ft_boolean.h
@@ -24,6 +24,6 @@ typedef enum e_bool
 # define EVEN_MSG "I have an even number of arguments."
 # define ODD_MSG "I have an odd number of arguments."
 # define SUCCESS 0
-# define EVEN(number) (number % 2 == 0) ? 1 : 0
+# define EVEN(number) ((number % 2) == 0)
 
 #endif
--- ex02/ft_abs.h
+++ ex02/ft_abs.h
@@ -13,6 +13,6 @@
 #ifndef FT_ABS_H
 # define FT_ABS_H
 
-# define ABS(Val) ((Val < 0) ? -Val : Val)
+# define ABS(Value) ((Value) * (((Value) > 0) - ((Value) < 0)))
 
 #endif
